2984191ce0f00674f42708e6d9e8be2b3cbd5926
Ale Gómez <gom20347@uvg.edu.gt>
2021-06-03 11:44:58 -0600

    Cambios en JavaScript

---
+++ src/main/webapp/js/Conexion.js
@@ -0,0 +1,48 @@
+
+/**
+ * Método para conectar base de datos.
+ * Driver.
+ */
+var driver = neo4j.driver(
+    'neo4j://http://94.74.64.91:7474',
+    neo4j.auth.basic('neo4j','Uvgenios2021')
+)
+
+/**
+ * Método de búsqueda
+ */
+function Search() {
+    var opcElegida = [] // Lista en donde se almacenan las opciones elegidas
+    document.querySelectorAll('button')
+
+    for (let i=0; i< opcRestaurante.length; i++) {
+        if (opcRestaurante[i].clicked) {
+            opcElegida.push(opcRestaurante[i].value)
+        }
+    } 
+    
+}
+
+/**
+ * 
+ */
+function borrar() {
+    // var nombre = document.getElementById("nombreBorrarTxt").value
+    // Conexión con base de datos
+    var session = driver.session()
+
+    session
+    .run('MATCH (n) WHERE n.name = $nombreParam DELETE n', { // Verificar opc
+        nombreParam: nombre
+    })
+    .then(result => {
+        alert("Se borró el juego ") // Error
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    .then(() => {
+        session.close()
+        console.log("Session closed")
+    })
+}
